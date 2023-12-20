a64050d4f7f90129ddd69d95f738dfd1ff9d3602
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-20 12:19:21 +0530

    added the new file

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jf1b*5t8!ufn96eh4yrkb%c8+e)t(7-%61k@q-94*dlk5zzm*s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-13-51-177-203.eu-north-1.compute.amazonaws.com'
+'127.0.01'
+]
 
 
 # Application definition
@@ -118,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
