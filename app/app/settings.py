924a6e382ce7d71e98810abb44a5c9637fbdf446
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2023-12-20 12:40:48 +0530

    added the file

--- app/app/settings.py
+++ app/app/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'healthapp'
 
 STATIC_ROOT = '/static/'
 
